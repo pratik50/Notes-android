38ba22bdd6d2e91daf80e6f0f6c1521895a835ca
Pratik <j.pratik025@gmail.com>
2023-05-19 17:45:20 +0530

    No changes added

---
+++ app/src/main/java/com/example/notes/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.notes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+
+class MainActivity : AppCompatActivity(), INotesRVAdapter {
+
+    lateinit var viewModel: NoteViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        val adapter = NotesRVAdapter(this, this)
+        recyclerView.adapter = adapter
+
+        viewModel= ViewModelProvider(this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)).get(NoteViewModel::class.java)
+        viewModel.allNotes.observe(this, Observer {list ->
+            list?.let {
+                adapter.updateList(it as ArrayList<Note>)
+            }
+        })
+
+    }
+    override fun onItemClicked (note: Note) {
+
+    }
+}
